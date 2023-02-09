2e70c8c9a07eb35902543b21da2d0a1c4c77387b
Claudio Marchiori <claudio.marchiori@codiceplastico.com>
2023-02-09 18:00:38 +0100

    Activate the buttons to manage task item

--- projects/mylist/src/app/views/dashboard/dashboard.component.ts
+++ projects/mylist/src/app/views/dashboard/dashboard.component.ts
@@ -10,19 +10,17 @@ import { Task } from '../../models/list.model';
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-  public tasks?: Task[];
+  public tasks: Task[] = [
+    { id: uuidv4(), title: 'Imparare Angular', done: false },
+    { id: uuidv4(), title: 'Imparare HTML', done: true },
+    { id: uuidv4(), title: 'Imparare CSS', done: true },
+  ];
 
   public model: string = '';
 
   constructor() {}
 
-  ngOnInit(): void {
-    this.tasks = [
-      { id: uuidv4(), title: 'Imparare Angular', done: false },
-      { id: uuidv4(), title: 'Imparare HTML', done: true },
-      { id: uuidv4(), title: 'Imparare CSS', done: true },
-    ];
-  }
+  ngOnInit(): void {}
 
   addTask() {
     const newTask: Task = {
@@ -31,7 +29,7 @@ export class DashboardComponent implements OnInit {
       done: false,
     };
 
-    this.tasks?.push(newTask);
+    this.tasks = [...this.tasks, newTask];
   }
 
   handleSubmit() {
@@ -40,10 +38,31 @@ export class DashboardComponent implements OnInit {
   }
 
   handleMarkTaskAsComplete(task: Task) {
-    console.log(`${this.constructor.name} - handleMarkTaskAsComplete()`, task);
+    if (task.done === true) {
+      console.warn(`The task was just complete!`);
+      return;
+    }
+
+    const confirm = window.confirm(`Sei sicuro di aver completato l'attività?`);
+    // console.log(`${this.constructor.name} - handleMarkTaskAsComplete()`, task, confirm);
+
+    if (confirm === true) {
+      const taskToUpdateIndex = this.tasks.findIndex((t) => t.id === task.id);
+
+      if (taskToUpdateIndex !== -1) {
+        this.tasks.splice(taskToUpdateIndex, 1, { ...this.tasks[taskToUpdateIndex], done: true });
+      }
+    }
   }
 
   handleDeleteTask(task: Task) {
-    console.log(`${this.constructor.name} - handleDeleteTask()`, task);
+    const confirm = window.confirm('Sei sicuro di voler procedere?');
+    // console.log(`${this.constructor.name} - handleDeleteTask()`, task, confirm);
+
+    if (confirm === true) {
+      const newTaskList = this.tasks.filter((t) => t.id !== task.id);
+
+      this.tasks = newTaskList;
+    }
   }
 }
